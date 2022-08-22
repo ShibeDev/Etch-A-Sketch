db2491f3f493b8b45605f49ca8b9577942b5c2ee
ShibeDev <echaruhi@gmail.com>
2022-08-22 00:27:39 -0700

    migrate grid coloring to reusable functions, implement eraser logic

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var color = "black";
 
+
 function removeAllChildNodes(parent){
     while(parent.firstChild){
         parent.removeChild(parent.firstChild);
@@ -13,26 +14,50 @@ function makeSquareGrid(col){
         let cell = document.createElement('div');
         content.appendChild(cell).className = 'grid-items';
     };
+    initialzieGrid();
 };
 
 function colorGrid(e){
     e.target.style.backgroundColor = color;
 }
 
+function initialzieGrid()
+{
+    grids = Array.from(document.querySelectorAll('.grid-items'));
+    grids.forEach(grid => grid.addEventListener('mouseenter',colorGrid));
+}
+
+function resetGrid()
+{
+    makeSquareGrid(16);
+    color = "black";
+    slider.value = 16;
+    sliderVals.forEach(sliderval => sliderval.textContent = slider.value);
+}
+
 const content = document.querySelector('#rightPanel');
-
-
 var slider = document.getElementById("myRange");
 const sliderVals = Array.from(document.querySelectorAll('.sliderVal'));
 sliderVals.forEach(sliderval => sliderval.textContent = slider.value);
 makeSquareGrid(16);
-var grids = Array.from(document.querySelectorAll('.grid-items'));
-grids.forEach(grid => grid.addEventListener('mouseenter',colorGrid));
+const buttons = Array.from(document.querySelectorAll('button'));
+buttons.forEach((button) => {
+    button.addEventListener('click' , () => {
+        switch(button.id) {
+            case "1":
+                resetGrid();
+              break;
+            case "2":
+                color = "white";
+              break;
+            default:
+              break;
+          }
+    });
+});
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
     sliderVals.forEach(sliderval => sliderval.textContent = slider.value);
     makeSquareGrid(slider.value);
-    grids = Array.from(document.querySelectorAll('.grid-items'));
-    grids.forEach(grid => grid.addEventListener('mouseenter',colorGrid));
 }
