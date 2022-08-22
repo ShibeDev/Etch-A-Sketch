60012591fa1c421b85dac6befa3bfe51cf956444
ShibeDev <echaruhi@gmail.com>
2022-08-21 23:39:23 -0700

    add function to reset grid and dynamically change size according to slider value

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 var color = "black";
 
+function removeAllChildNodes(parent){
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
+}
+
 function makeSquareGrid(col){
+    removeAllChildNodes(content);
     content.style.gridTemplateColumns= `repeat(${col},auto)`;
     for(i = 0; i < (col* col); i++){
         let cell = document.createElement('div');
@@ -19,11 +26,13 @@ var slider = document.getElementById("myRange");
 const sliderVals = Array.from(document.querySelectorAll('.sliderVal'));
 sliderVals.forEach(sliderval => sliderval.textContent = slider.value);
 makeSquareGrid(16);
-const grids = Array.from(document.querySelectorAll('.grid-items'));
+var grids = Array.from(document.querySelectorAll('.grid-items'));
 grids.forEach(grid => grid.addEventListener('mouseenter',colorGrid));
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
     sliderVals.forEach(sliderval => sliderval.textContent = slider.value);
-    //makeSquareGrid(slider.value);
+    makeSquareGrid(slider.value);
+    grids = Array.from(document.querySelectorAll('.grid-items'));
+    grids.forEach(grid => grid.addEventListener('mouseenter',colorGrid));
 }
